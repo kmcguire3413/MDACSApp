fd4c7f1130a03fc4b24c19f6719d493f68c8862c
Leonard Kevin McGuire Jr <leonard.kevin.mcguire.jr@gmail.com>
2018-05-19 04:05:19 -0500

    travis-ci prep and cleanup

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using MDACS.Server;
+using System.Diagnostics;
 
 namespace MDACS.App
 {    
--- jsxcompile.py
+++ jsxcompile.py
@@ -1,51 +1,18 @@
+#!python3
 import subprocess
 import xml.etree.ElementTree as et
 import datetime
 import json
 import os.path
 import os
+import sys
 
-def IncrementVersionString(verstr):
-	verstr = verstr.split('.')
-
-	major = int(verstr[0])
-	minor = int(verstr[1])
-	build = int(verstr[2])
-	rev = int(verstr[3])
-
-	dt = datetime.date.today()
-	# The version is split into two 16-bit fields.
-	# major.minor.YMM.DDRRR
-	build = (dt.year - 2016) * 100 + dt.month
-	rev_rrr = rev - (rev // 1000 * 1000)
-	rev = dt.day * 1000 + (rev_rrr + 1)
-
-	return '%s.%s.%s.%s' % (major, minor, build, rev)
-
-def IncrementVersionOnProject(breaking_changes=False):
-	buildinfo_path = 'buildinfo.json'
-	if os.path.exists(buildinfo_path):
-		fd = open(buildinfo_path, 'r')
-		buildinfo = json.loads(fd.read())
-		fd.close()
-	else:
-		buildinfo = {
-			'version': '0.0.0.0',
-		}
-
-	buildinfo['version'] = IncrementVersionString(buildinfo['version'])
-
-	fd = open(buildinfo_path, 'w')
-	fd.write(json.dumps(buildinfo))
-	fd.close()
-
-print('+ incrementing version')
-IncrementVersionOnProject()
+babelBinary = sys.argv[1]
 
 def jsx_to_js(infile, outputs):
 	print('+ compiling JSX into JS for %s' % infile)
 	stdout, stderr = subprocess.Popen([
-			'babel',
+			babelBinary,
 			'--plugins',
 			'transform-react-jsx',
 			infile,
