174157ea04a9459c53373976393865c8b6106a5c
Leonard Kevin McGuire Jr <leonard.kevin.mcguire.jr@gmail.com>
2018-05-19 21:51:22 -0500

    Added more debug support for babel execution failure with 'jsxcompile.py'.

--- MDACSApp/jsxcompile.py
+++ MDACSApp/jsxcompile.py
@@ -10,15 +10,20 @@ import sys
 os.chdir(sys.argv[1])
 babelBinary = sys.argv[2]
 
+print('Working Directory = %s' % sys.argv[1])
+
 def jsx_to_js(infile, outputs):
 	print('+ compiling JSX into JS for %s' % infile)
-	stdout, stderr = subprocess.Popen([
-			babelBinary,
-			'--plugins',
-			'transform-react-jsx',
-			infile,
-	], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate('')
-
+	try:
+		stdout, stderr = subprocess.Popen([
+				babelBinary,
+				'--plugins',
+				'transform-react-jsx',
+				infile,
+		], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate('')
+	except FileNotFoundError as ex:
+		print('CWD=%s' % os.getcwd())
+		raise ex
 	stderr = stderr.decode('utf8')
 	stdout = stdout.decode('utf8')
 
@@ -63,18 +68,18 @@ def compile_jsx_and_concat(pdir, odir):
 			pd.append(
 				(os.path.join(odir, 'daos.js'), 'a')
 			)
-		
+
 		jsx_to_js(
-			os.path.join(pdir, node), 
+			os.path.join(pdir, node),
 			pd
 		)
-	
+
 	if os.path.exists(os.path.join(pdir, 'app.jsx')):
 		jsx_to_js(
-			os.path.join(pdir, 'app.jsx'), 
+			os.path.join(pdir, 'app.jsx'),
 			[
 				(os.path.join(odir, 'package.js'), 'a'),
 			]
-		)	
+		)
 
 compile_jsx_and_concat('./websrc', './webres')
